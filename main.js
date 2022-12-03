5fac0fdc669552de3e40bbd2ce8385ef8034527a
CITLALLI24ROMERO <119757853+CITLALLI24ROMERO@users.noreply.github.com>
2022-12-03 14:52:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let app = document.getElementById('typewriter');
+
+let typewriter = new typewriter(app, {
+loop: true,
+delay: 75,
+});
+
+typewriter
+.pauseFor(2500)
+.typeString(`Se da a conocer la carrera y proyectos que se elaboran.`)
+.pauseFor(200)
+.deleteChars(10)
+.start();
+Footer
+© 2022 GitHub,Inc.
+Footer navigation
+Terms 
+Privacy
+Segurity
+
